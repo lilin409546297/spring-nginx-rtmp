9149b8d540b3d17bc4bbae32fcb78e30da95d754
lilin <lilin409546297@*>
2019-09-24 18:31:09 +0800

    init

--- src/main/java/com/cetc/ffmpeg/InitCamera.java
+++ src/main/java/com/cetc/ffmpeg/InitCamera.java
@@ -18,33 +18,30 @@ public class InitCamera implements CommandLineRunner{
     public void run(String... args) throws Exception {
         String shell = "ffmpeg -rtsp_transport tcp -i 'rtsp://admin:admin1234@192.168.112.252:554/cam/realmonitor?channel=1&subtype=0' -stimeout '3000000' -vcodec copy -acodec copy -f flv -y 'rtmp://localhost:1935/hls/test'";
         String[] cmd = new String[] {"sh", "-c", shell};
-        new Thread(new Runnable() {
-            ThreadLocal<String[]> threadLocal = new ThreadLocal<>();
-            @Override
-            public void run() {
-                threadLocal.set(cmd);
-                while (true) {
-                    try {
-                        Process process = Runtime.getRuntime().exec(threadLocal.get());
-                        new Thread(() -> {
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-                            String str;
-                            try {
-                                logger.info("start");
-                                while ((str = bufferedReader.readLine()) != null) {
-                                    logger.info(str);
-                                }
-                                logger.info("exit");
-                                process.exitValue();
-                            } catch (IOException e) {
-                                e.printStackTrace();
+        ThreadLocal<String[]> threadLocal = new ThreadLocal<>();
+        new Thread(() -> {
+            threadLocal.set(cmd);
+            while (true) {
+                try {
+                    Process process = Runtime.getRuntime().exec(threadLocal.get());
+                    new Thread(() -> {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                        String str;
+                        try {
+                            logger.info("start");
+                            while ((str = bufferedReader.readLine()) != null) {
+                                logger.info(str);
                             }
-                        }).start();
-                        process.waitFor();
-                        logger.info("ffmpeg restart");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                            logger.info("exit");
+                            process.exitValue();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+                    process.waitFor();
+                    logger.info("ffmpeg restart");
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }).start();
